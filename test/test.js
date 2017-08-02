9057e7db169b30a8cea94a1ff92eb396eba19a72
sofiat <abioye.sofiat@outlook.com>
2017-08-02 12:42:23 +0100

    changes to my files

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ var aritGeo = require('../lab1.js');
     describe("Case for empty array", function() {
 
       it("should return 0 for an empty array", function() {
-        assert(aritGeo([]), 0);
+       assert.equal(aritGeo([]), 0);
       });
 
     });
@@ -15,6 +15,9 @@ var aritGeo = require('../lab1.js');
 
       it("should return 'Arithmetic' for [2, 4, 6, 8, 10]", function() {
         assert(aritGeo([3, 6, 9, 12]), 'Arithmetic');
+      });
+      it("should return 'Arithmetic' for [1, 2, 3, 4, 5]", function() {
+        assert(aritGeo([1,2,3,4,5,6]), 'Arithmetic');
       });
 
       it("should return 'Arithmetic' for [5, 11, 17, 23, 29, 35, 41]", function() {
@@ -39,7 +42,10 @@ var aritGeo = require('../lab1.js');
     describe("Case for neither Arithmetic nor Geometric Sequence", function() {
 
       it("should return -1 for [1, 2, 3, 5, 8]", function() {
-        assert(aritGeo([1, 2, 3, 5, 8]), -1);
+        assert(aritGeo([1, 2, 3, 7, 8]), -1);
+      });
+      it("should return -1 for [1, 2, 3, 5, 8]", function() {
+        assert(aritGeo([2, 4, 7, 14]), -1);
       });
 
       it("should return -1 for [1, 3, 6, 10, 15]", function() {
