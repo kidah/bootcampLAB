5051e27626a21e6d44c7211a7b6f2c69bb0958fa
sofiat <abioye.sofiat@outlook.com>
2017-08-04 17:20:29 +0100

    fix algorithm

--- lab1.js
+++ lab1.js
@@ -1,27 +1,35 @@
- var aritGeo = function minaritGeo(items){
-    arithdiff = items[1] - items[0];
-    geodiff = items[1] / items[0];
-    if (items.length == 0 ){
+'use strict';
+let aritGeo = (array) => {
+  const arithdiff = array[1] - array[0];
+  const geodiff = array[1] / array[0];
+  let isArit = true; let isGeo = true;
+
+  for(let i = 0; i < array.length - 1; i++)
+  {
+    if(array[i+1] - array[i] !== arithdiff){
+      isArit = false;
+    }
+    if(array[i+1] / geodiff !== array[i]){
+      isGeo = false;
+    }
+  }
+
+    if (Array.isArray(array) === false){
+      return -1;
+    }
+    else if (array.length === 0){
       return 0;
     }
     else{
-      for (var i=2; i< items.length; i++){
-       if((items[i] - items[i-1]) !== arithdiff && items.length > 2){
-         for (var i=2; i< items.length; i++){
-           if((items[i] / items[i-1]) !== geodiff && items.length > 2){
-            return -1;
-           }
-           else{
-             return 'Geometric';
-           }
-         }
-       }
-       else {
-        return 'Arithmetic';
-      }
+            if(isArit === true){
+              return "Arithmetic";
+            }
+            else if(isGeo === true){
+              return "Geometric";
+            }
+             else{
+              return -1;
+             }
     }
-  }
- 
-    
 }
 module.exports = aritGeo;
